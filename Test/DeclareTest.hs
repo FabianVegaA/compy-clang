b84ac0c6a7954653541edcf97baad27e4bb7386a
FabianVegaA <fabianveal@gmail.com>
2022-05-21 18:33:03 -0400

    test(Declare): add declare test

---
+++ Test/DeclareTest.hs
@@ -0,0 +1,16 @@
+module Test.DeclareTest where
+
+import Expression
+  ( Expression (Declare, Infix, Literal),
+    Operator (Add),
+    Type (Int),
+  )
+import Test.UtilsTest
+
+declareTest :: IO Bool
+declareTest =
+  let cases =
+        [ (Declare "x" (Literal "1" Int) Int, "int x=1;"),
+          (Declare "x" (Infix (Literal "1" Int) Add (Literal "1" Int)) Int, "int x=1+1;")
+        ]
+   in test cases "Declare"
